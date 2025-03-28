607e3e531cba488682c5633d4ac2d1d8d0eb3b59
SanchayKanade <sanchay2598@gmail.com>
2025-03-28 16:36:31 -0500

    Dbcontext, dependancy injection and db migrations done

---
+++ PaymentRegister/PaymentRegister/Models/PaymentDetailContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+
+namespace PaymentRegister.Models
+{
+    public class PaymentDetailContext : DbContext
+
+    {   //Dependency injection required here for DbContextOptions which we will do via Program.cs
+        public PaymentDetailContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails {get; set;}
+    }
+}
